689016693f74449be6c833d137453085dcfcbd09
Julián Aragón <juliaragonm@hottmail.com>
2021-04-15 23:37:44 -0500

    p6 update 1

--- práctica 6/transmitter.v
+++ práctica 6/transmitter.v
@@ -1,13 +1,16 @@
 module transmitter (
 input [7:0]idata,
 input iEN,
-input iBPS
+//input iBPS,
 input iClk,
 output odata
 );
 
-reg [7:0]rdata_D;
-reg [7:0]rdata_Q;
+reg [7:0]rinput_D;
+reg [7:0]rinput_Q;
+
+reg rdata_D = 1'd1;
+reg rdata_Q;
 
 reg [2:0]rstate_D;
 reg [2:0]rstate_Q;
@@ -15,72 +18,81 @@ reg [2:0]rstate_Q;
 reg [3:0]rcounter_D;
 reg [3:0]rcounter_Q;
 
-reg rparity_D;
-reg rparity_Q;
+reg [7:0]rparity_D;
+reg [7:0]rparity_Q;
 
-reg rstart_D;
-reg rstart_Q;
 
 assign odata = rdata_Q;
 
 initial 
 begin
-	rstart_D = 1'd1;
+	rdata_D = 1'd1;
 end
 
-always @ (iClk)
+always @ (posedge iClk)
 begin
 	rcounter_Q <= rcounter_D;
-	rstart_Q <= rstart_D;
 	rparity_Q <= rparity_D;
-if(iBPS)
+	rinput_Q <= rinput_D;
+//if(iBPS)
 	rdata_Q <= rdata_D;
-else	
-	rdata_Q <= rdata_Q;
-end
+//else
+//	rdata_Q <= rdata_Q;
+//end
 
 always @ * 
 begin
 	case(rstate_Q)
 	2'd0: //IDLE
 	begin
-		rdata_D = idata;
-		rcounter_D = 3'd0;
 		if(iEN)
 		begin
+		rinput_D = idata;
 		rstate_D = 2'd1;
 		end
 	end
 	2'd1: //START
 	begin
-		rstart_D = 1'd0;
+		rdata_D = 1'd0;
 		rstate_D = 2'd2;
 	end
 	2'd2: //DATA
 	begin
 		if(rcounter_Q == 3'd7)
 		begin
-			rstate_D = 2'd3;
+			if (rparity_D % 2 == 1) // paridad par o impar
+			begin
+				rdata_D = 1'd0; //impar
+			end
+			else
+			begin
+				rdata_D = 1'd1; //par
+			end
+			rcounter_D = 3'd0; 
+			rstate_D = 2'd3; //sig edo.
 		end
 		else
 		begin
+			rdata_D = rinput_D[rcounter_Q];
 			rcounter_Q = rcounter_D + 3'd1;
-			rdata_D = idata[rcounter_Q];
+			if(rdata_D == 1'd1)
+			begin
+				rparity_Q = rparity_D + 1'd1;
+			end
+			else
+			begin
+				rparity_Q = rparity_D;
+			end
 		end
 	end
-	2'd3: //STOP
-	begin
-		if(rdata_D % 2)
+		2'd3: //STOP
 		begin
-			rparity_Q = 1'd0;
+			rdata_D = 1'd0;
+			rstate_D = 3'd0;
+		default:
+		begin
+			rdata_D = 1'd1;
 		end
-		rstart_D = 1'd1;
-		rstate_D = 2'd0;
-	end
-	default:
-	begin
-		rstart_D = 1'd1;
-	end
 	endcase
 end
 
